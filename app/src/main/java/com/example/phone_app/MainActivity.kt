4c67ad7c00195faf8e21ad4d7c11e083b7a25315
constantinos <constantine_flds@hotmail.com>
2019-07-17 00:26:17 +0300

    Geting data with voley

--- app/src/main/java/com/example/phone_app/MainActivity.kt
+++ app/src/main/java/com/example/phone_app/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.phone_app
 
+import android.app.DownloadManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_data.setOnClickListener{
+            val serverUrl:String = "https://192.168.1.1/e_com/present_json_array.php"
+            var requestQ: RequestQueue = Volley.newRequestQueue(this@MainActivity)
+            var stringRequst = StringRequest(Request.Method.GET,serverUrl,Response.Listener { response ->  showdata.text=response},
+                Response.ErrorListener { error-> showdata.text=error.message})
+             requestQ.add(stringRequst)
+        }
     }
 }
